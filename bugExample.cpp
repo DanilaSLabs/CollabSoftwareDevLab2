43b431f57936c00de6dc61928ae26360ed4fcf0f
Daniil <stankevich.d@live.unic.ac.cy>
2026-03-18 13:52:42 +0200

    Bug in line 130 and line 131 fixed

--- bugExample.cpp
+++ bugExample.cpp
@@ -127,8 +127,8 @@ void fillInArray(int arr[], const int size){
 void multArrays(const int arrQuantity[], const int arrPrice[], int arrTotal[], const int size){
   assert (size > 0);
 
-  for (int i = 0; i <= size; ++i){
-    arrTotal[i] = arrQuantity[i] + arrPrice[i+1];
+  for (int i = 0; i < size; ++i){
+    arrTotal[i] = arrQuantity[i] * arrPrice[i];
   }
 }
 
